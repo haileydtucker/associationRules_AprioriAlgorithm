52865b964d01cd9a3bfe64cbc3097831938ff695
haileydtucker <68257307+haileydtucker@users.noreply.github.com>
2020-10-05 16:38:32 -0400

    analysis of association rules for country genre

--- associationRules.py
+++ associationRules.py
@@ -79,5 +79,67 @@ for s in range(len(all_songs)):
     print()
     print()
             
+#Reading in 5 songs from Country Genre 
+with open("song6.txt") as f:
+    song_6 = []
+    words = f.read().lower().splitlines()
+    for i in range(len(words)):
+        line_split = words[i].split()
+        song_6.append(line_split)
+        
+with open("song7.txt") as f:
+    song_7 = []
+    words = f.read().lower().splitlines()
+    for i in range(len(words)):
+        line_split = words[i].split()
+        song_7.append(line_split)
+
+with open("song8.txt") as f:
+    song_8 = []
+    words = f.read().lower().splitlines()
+    for i in range(len(words)):
+        line_split = words[i].split()
+        song_8.append(line_split)
+        
+with open("song9.txt") as f:
+    song_9 = []
+    words = f.read().lower().splitlines()
+    for i in range(len(words)):
+        line_split = words[i].split()
+        song_9.append(line_split)
+        
+with open("song10.txt") as f:
+    song_10 = []
+    words = f.read().lower().splitlines()
+    for i in range(len(words)):
+        line_split = words[i].split()
+        song_10.append(line_split)
+        
+all_songs1 = []
+all_songs1.append(song_6)
+all_songs1.append(song_7)
+all_songs1.append(song_8)
+all_songs1.append(song_9)
+all_songs1.append(song_10)
+
+#Running apiori - Country
+for s in range(len(all_songs1)):
+    count = s + 1
+    itemsets = apriori(all_songs1[s], min_support=0.4, min_confidence=0.2, min_lift=.1)
+    # itemsets is a Python generator. We can for-loop through it,
+    # or force it to fill in by making it a list.
+    itemsets = list(itemsets)
+    # Then we can check its length
+    print('SONG', count, ' : ', 'Found', len(itemsets), 'frequent itemsets for song', count, 'of Country Music.')
+    # loop through the list of itemsets
+    for i, rec in enumerate(itemsets):
+        items = list(rec.items) # convert from Python frozenset to list
+        print('Itemset {} has support {} with items {}'.format(i, rec.support, items))
+    # loop through the rules found for this itemset
+        for j, rule in enumerate(rec.ordered_statistics):
+            print(' {} --> {} confidence: {:.2f} lift: {:.2f}'.format(list(rule.items_base),list(rule.items_add), rule.confidence, rule.lift))
+    print()
+    print()
+            
 
  
